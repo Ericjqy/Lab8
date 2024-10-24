74f0fbdb969ffc95731004cc63cf8dad2fe94358
Qingyun Jia <qjia2@ualberta.ca>
2024-10-24 10:04:46 -0600

    failing test for delete

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -63,6 +63,7 @@ public class CustomList extends ArrayAdapter<City> {
     public boolean hasCity(City city){
         return cities.contains(city);
     }
-
+    public void delete(City city){
+    }
 
 }
