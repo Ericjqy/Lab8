5d9453ed867e09484dc8c2cf734699ffe941a2b6
Qingyun Jia <qjia2@ualberta.ca>
2024-10-24 09:56:56 -0600

    hasCity failing build

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -28,6 +28,14 @@ public class CustomListTest {
         assertEquals(list.getCount(),listSize + 1);
     }
 
+    @Test
+    public void hasCityTest(){
+        list = MockCityList();
+        City city = new City("Estevan", "SK")
+        list.addCity(city);
+        assertEquals(list.hasCity(city), true);
+    }
+
 
 
 }
